153a1d86ba93d71e27b60303491693c8ecdc61d8
Silvia Lupsa <lupsasilvia@gmail.com>
2024-01-25 19:25:38 +0200

    get started

---
+++ src/test/java/com/glovopartners/glovo/GlovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glovopartners.glovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
